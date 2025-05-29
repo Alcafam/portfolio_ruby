c659737076474f5ecde2337a74fab0c44a7f25ce
AlcaFam <galinganmarjy61@gmail.com>
2025-05-29 17:04:46 +0800

    Portfolio page & resume

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,3 +4,14 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join("app/assets/documents")
+Rails.application.config.assets.accept = [
+  "text/css",
+  "application/javascript",
+  "application/json",
+  "image/png",
+  "image/jpeg",
+  "image/gif",
+  "image/svg+xml",
+  "application/pdf"  # ✅ Allow serving PDFs
+]
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get "projects", to: "projects#index"
+  # get "projects/show"
+  # get "projects/new"
   get "/", to: "homepage#home", as: "root"
 end
