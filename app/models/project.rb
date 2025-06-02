545836d0dc00aa9bc69356852d9bdf1617f74bab
AlcaFam <galinganmarjy61@gmail.com>
2025-06-03 00:11:01 +0800

    view project details

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ApplicationRecord
     has_many :taggings, dependent: :destroy
     has_many :tags, through: :taggings
+
+    validates :slug, presence: true, uniqueness: true
+
+    def to_param
+        slug
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "projects", to: "projects#index"
-  # get "projects/show"
+  get "projects/:slug", to: "projects#show", as: :project
   # get "projects/new"
   get "/", to: "homepage#home", as: "root"
 end
